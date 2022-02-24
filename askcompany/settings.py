3a45f673463a0a652b7dfe5c8b9ddd04fbb34e85
xalphee <zalphee@gmail.com>
2022-02-24 15:19:55 +0900

    instagram 앱 추가...

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # 3rd 패키지
     'rest_framework',
+
+    # 추가 App
+    'instagram',
+
+
 ]
 
 MIDDLEWARE = [
