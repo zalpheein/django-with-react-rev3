88ca2ee447b6fac540443e4e1b405c5cd4fb3daa
zalphee <zlaphee@gmail.com>
2022-03-01 17:43:39 +0900

    추가된 author 필드도 직열화 하여 반환...

--- instagram/serializers.py
+++ instagram/serializers.py
@@ -1,8 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='author.username')
+    
     class Meta:
         model = Post
-        fields = "__all__"
+        # fields = "__all__"
+        fields = ['pk', 'username', 'message', 'created_at', 'updated_at']
