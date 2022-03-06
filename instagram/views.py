3bbcb80e31d623e4140643e7b48c848d69705e0f
xalphee <zalphee@gmail.com>
2022-03-06 13:15:16 +0900

    authentications_class 가 아니라... permission_class 임... 오류 수정...

--- instagram/views.py
+++ instagram/views.py
@@ -53,7 +53,7 @@ class PostViewSet(ModelViewSet):
     # 아래와 같은 지정이 없을 경우,
     # 아래의 함수들(perform_create,public , set_public) 을 호출 할 수 없음
     # 즉, 인증이 되어 있음을 보장 받을수 있음
-    authentication_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     # dispatch 함수는 장고 클래스 기반 뷰에서 실제 요청이 될 때마다 호출 되는 함수
     # def dispatch(self, request, *args, **kwargs):
